142b80b581544687789a6312358c31b7485716be
Kevin Boston <kbos22@gmail.com>
2020-06-23 02:01:16 +0000

    tinkering and reducing scope

--- whis-cli/lib/whis/cli.rb
+++
@@ -1,8 +0,0 @@
-require "whis/cli/version"
-
-module Whis
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- whis-cli/lib/whis/cli/version.rb
+++
@@ -1,5 +0,0 @@
-module Whis
-  module Cli
-    VERSION = "0.1.0"
-  end
-end
